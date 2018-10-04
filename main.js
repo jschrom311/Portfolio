4172315c7f5cccd51df64478c58aa0c42ac8a781
GitGud <jschrom311@yahoo.com>
2018-10-03 20:05:10 -0500

    navbar complete-favicon added

--- main.js
+++ main.js
@@ -1,29 +1,5 @@
 'use strict';
 
-/*$(document).ready(main);
-function main() {
-  $('#headerimg').hide();
-  $('#headerimg').fadeIn(1500);
-}
-
-$('#about').click(function() {
-  $('html, body').animate({
-    scrollTop: $('#about').offset().top
-  }, 800);
-});
-
-$('#projects').click(function() {
-  $('html, body').animate({
-    scrollTop: $('#project').offset().top
-  }, 800);
-});
-
-$('#contact').click(function() {
-  $('html, body').animate({
-    scrollTop: $('#contact').offset().top
-  }, 800);
-});*/
-
 $(document).ready(function() {
     $('a[href*=#]').bind('click', function(e) {
             e.preventDefault(); // prevent hard jump, the default behavior
